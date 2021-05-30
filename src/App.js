c259b3dfc96a2b0e3503858b2a4924d5e34d64d1
Ajil Abdalqader <ajilqado@gmail.com>
2021-05-30 17:33:31 +0200

    passing-props-to-SingleColor-&&-join()-to-display-stringValue

--- src/App.js
+++ src/App.js
@@ -5,42 +5,48 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState('')
   const [error, setError] = useState(false)
-  const [list, setList] = useState([])
+  const [list, setList] = useState(new Values('#f15025').all(10))
 
   const handleSubmit= (e) =>{
     e.preventDefault()
   
-    try {
-        let colors = new Values(color).all(10)
-  
-    } catch (error) {
-      setError(true)
-      console.log(error)
-    }
+      try {
+       let colors = new Values(color).all(10)
+        setColor(colors)
    
-  }
+          } catch (error) {
+            setError(true)
+            console.log(error)
+          }
+  } 
   
   return (
   <> 
 
-  <section className="container">
- <h3>color Generator</h3>
+<section className="container">
+  <h3>color Generator</h3>
  
- <form onSubmit={handleSubmit}>
+  <form onSubmit={handleSubmit}>
 
- <input type="text" value={color} onChange={(e)=>{
-   setColor(e.target.value)
- }}
- placeHolder="#f3333"
-  // className={`${error ? 'error' : null}`}
- />
+      <input type="text" value={color} onChange={(e)=>{
+        setColor(e.target.value)
+        }}
+        placeholder="#f3333"
+        className={`${error ? 'error' : null}`}
+        />
 
- <button className="btn" type= "submit" >submit</button>
- </form>
+      <button className="btn" type= "submit" >submit</button>
 
-  </section>
-    <section className="colors">
-  </section>
+   </form>
+
+</section>
+
+<section className="colors">
+  {list.map((color,index)=>{
+    console.log(color)
+    return <SingleColor {...color} index={index}/>
+  })}
+</section>
 
   </> 
   )
