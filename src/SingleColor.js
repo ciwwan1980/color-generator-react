18f947f6eef5a07d31e72fc32bcd85f336168e3b
Ajil Abdalqader <ajilqado@gmail.com>
2021-05-30 18:41:57 +0200

    useEffect-whenever-the-valueIsChanged-clear-clipboard-after-setTimeOut

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -6,14 +6,28 @@ const SingleColor = ({rgb,weight, index, hexColor}) => {
     const [alert, setAlert] = useState(false)
     const bcg = rgb.join(',')
     const hex = rgbToHex(...rgb)
+    const hexValue = `#${hexColor}`
+
+    useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false)
+    }, 4000)
+    return () => clearTimeout(timeout)
+  }, [alert])
   
    return <article 
        className={`color ${index > 10 && 'color-light'}`}
         style={{ backgroundColor: `rgb(${bcg})` }}
+        onClick={() => {
+        setAlert(true)
+        navigator.clipboard.writeText(hexValue)
+      }}
    >
 
   <p>{hexColor}</p>
   <p className="percent-value">{weight}%</p>
+  <p className='color-value'>{hexValue}</p>
+    {alert && <p className='alert'>copied to clipboard</p>}
   </article>
 }
 
