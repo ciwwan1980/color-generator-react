8fa091801b3c22bd0145e6d2624a831e771542b5
Ajil Abdalqader <ajilqado@gmail.com>
2021-06-30 11:46:29 +0200

    commit

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
 const SingleColor = ({rgb,weight, index, hexColor}) => {
-  console.log(hexColor, "hexColor")
+  // console.log(hexColor, "hexColor")
     const [alert, setAlert] = useState(false)
+
     const bcg = rgb.join(',')
-    const hex = rgbToHex(...rgb)
+    // const hex = rgbToHex(...rgb)
+    
     const hexValue = `#${hexColor}`
 
     useEffect(() => {
